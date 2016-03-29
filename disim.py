af09b356be79fe1c05a195f93712152082ec92ab
tejas pitkar <tejas2kxv@gmail.com>
2016-03-29 23:23:52 +1000

    updated 1
    added new variable names and extended range of the numbers from 0 to 99.
    
    5 guesses allowed.

--- disim.py
+++ disim.py
@@ -1,34 +1,29 @@
 import random
 
-token = 0
-number = random.randint(1,10)
+counter = 0
+guess = 0
+number = random.randint(0,99)
 print "Let's play a game!"
-print ('Enter and guess  a number between 1 and 10')
+print ('Enter and guess  a number between 1 and 99')
 
 
-while token < 3:
-    user = input()
-    user = int(user)
-    token += 1
-    if user < number:
+while counter < 5 and guess != number:
+    guess = input("Enter a number: "  )
+    
+    if guess < number:
         print ('a bit high')
-    if user > number:
+    elif guess > number:
         print ('a bit low')
-    if user == number:
-        break
-    if user in range(11,999999999):
-	    print "Enter number only between 1 and 10."
+    counter +=1 
+    
          	
-if user  == number:
-        token = str(token)
-        print ('you guess right!!')
-        print ('you guessed in ' + token + ' guesses' )
-        
-elif user != number:
-         number = str(number)
-         print ('i was thinking of ' + number )
+if guess == number:
+    print ('you guess right!!')
+    print ('you guessed in ' + counter + ' guesses' )
+
 else:
-         print ('invalid input')
+	print "No more guesses , you lost!"
+	print "The number was: %d" % number
             
             
 
